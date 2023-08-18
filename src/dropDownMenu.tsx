1fb717e41a43078dc6199156429a0ed8f6285723
7uppp <markwang9315@gmail.com>
2023-08-18 15:58:28 +1000

    update

--- src/dropDownMenu.tsx
+++ src/dropDownMenu.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import style from './dropDownMenu.module.scss'
-import downArrowIcon from '../../assets/images/downArrowIcon.svg'
+import downArrowIcon from './assets/images/downArrowIcon.svg'
 
 interface DropdownProps {
   options: string[]
